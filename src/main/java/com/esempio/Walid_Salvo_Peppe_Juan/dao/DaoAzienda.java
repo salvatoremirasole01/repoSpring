70523d6946e555ec35e20111e14619fbd17b457d
juanfarro <juan.farro92@gmail.com>
2020-07-30 15:56:47 +0200

    daoaziendajuan2

--- src/main/java/com/esempio/Walid_Salvo_Peppe_Juan/dao/DaoAzienda.java
+++ src/main/java/com/esempio/Walid_Salvo_Peppe_Juan/dao/DaoAzienda.java
@@ -1,6 +1,51 @@
 package com.esempio.Walid_Salvo_Peppe_Juan.dao;
 
-public class DaoAzienda {
+import com.ant.utility.model.dao.DaoDBMS;
+import com.ant.utility.model.db.IDatabase;
+import com.ant.utility.model.entities.Entity;
+import com.esempio.Walid_Salvo_Peppe_Juan.model.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
 
-    // bisogna caricare l'utility
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DaoAzienda extends DaoDBMS {
+
+    @Autowired
+    IDatabase db;
+
+
+    @Override
+    public List<Entity> list() {
+        List lista = new ArrayList<Entity>();
+        String query = "select * from azienda";
+
+        for(Map<String,String> mappa : db.rows(query)){
+            Entity e = new Persona();
+            e.fromMap(mappa);
+            lista.add(e);
+
+        }
+        return lista ;
+    }
+
+    @Override
+    public Entity search(int i) {
+        for(Entity w : list()){
+            if(w.getId() == i)
+                return w;
+        }
+        return null;
+    }
+
+    @Override
+    public Entity load(Entity entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int i) {
+        return false;
+    }
 }
