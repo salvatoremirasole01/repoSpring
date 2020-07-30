7fb08e7753642af98725cb173c4744845da1ee3b
juanfarro <juan.farro92@gmail.com>
2020-07-30 18:01:31 +0200

    daoaziendajuan3

--- src/main/java/com/esempio/Walid_Salvo_Peppe_Juan/dao/DaoAzienda.java
+++ src/main/java/com/esempio/Walid_Salvo_Peppe_Juan/dao/DaoAzienda.java
@@ -3,6 +3,7 @@ package com.esempio.Walid_Salvo_Peppe_Juan.dao;
 import com.ant.utility.model.dao.DaoDBMS;
 import com.ant.utility.model.db.IDatabase;
 import com.ant.utility.model.entities.Entity;
+import com.ant.utility.model.entities.EntityException;
 import com.esempio.Walid_Salvo_Peppe_Juan.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +12,8 @@ import java.util.List;
 import java.util.Map;
 
 public class DaoAzienda extends DaoDBMS {
+
+    private String table = "azienda";
 
     @Autowired
     IDatabase db;
@@ -40,12 +43,36 @@ public class DaoAzienda extends DaoDBMS {
     }
 
     @Override
-    public Entity load(Entity entity) {
-        return null;
+    public Entity load(Entity e) {
+        if (e.getId() != 0 && search(e.getId()) != null)
+        {
+            String modifiche = caricaMappa(e.toMap());
+            String query = UPDATE.replace("table", table).replace("[id]", e.getId() + "").replace("?", modifiche);
+
+
+            return search(db.execute(query));
+        } else {
+            String chiavi = caricaString(e.toMap().keySet()).replace("'", "");
+            String valori = caricaString(e.toMap().values());
+            String query = CREATE.replace("table", table).replace("chiavi", chiavi).replace("valori", valori);
+
+            int id = db.execute(query);
+            if (id > 0) // id valido
+            {
+                try {
+                    e.setId(id);
+                } catch (EntityException e1) {
+                    e1.printStackTrace();
+                } // riaggiorno l'oggetto con l'id nuovo
+
+                return search(e.getId());
+            }
+            return null;
+        }
     }
 
     @Override
-    public boolean delete(int i) {
-        return false;
+    public boolean delete(int id) {
+        return db.execute(DELETE.replace("table", table).replace("[id]", id + "")) == -200;
     }
 }
